4c62f7980fbf3e28fdb251ffd034d6360a46181e
Jerin Richard <jerinrichard@gmail.com>
2025-09-24 11:51:10 +0530

    change in route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () { return view('login');});
-
 Route::get('/', function () { return view('pages.index');})->name('home');
 Route::get('/about', function () { return view('pages.about');})->name('about');
 Route::get('/service', function () { return view('pages.service');})->name('service');
